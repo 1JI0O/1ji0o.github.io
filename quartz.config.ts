e42d6dcda66a5b5221cf6e8da78c02df2fa23c9a
1ji0o <38367560+1JI0O@users.noreply.github.com>
2026-01-28 13:01:08 +0800

    vault backup: 2026-01-28 13:01:08

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "1ji0o.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
